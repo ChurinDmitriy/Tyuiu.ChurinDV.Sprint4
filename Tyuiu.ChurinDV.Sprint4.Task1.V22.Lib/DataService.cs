ef2e766e52166326ea577b14512b03aa55cf94a8
ChurinDmitriy <mister.dim2017111@gmail.com>
2023-10-31 08:57:04 +0500

    Task1

---
+++ Tyuiu.ChurinDV.Sprint4.Task1.V22.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ChurinDV.Sprint4.Task1.V22.Lib
+{
+    public class DataService : ISprint4Task1V22
+    {
+        public int Calculate(int[] array)
+        {
+            int multiply = 1;
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] % 2 == 1)
+                {
+                    multiply *= array[i];
+                }
+            }
+            return multiply;
+        }
+    }
+}
---
+++ Tyuiu.ChurinDV.Sprint4.Task1.V22.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ChurinDV.Sprint4.Task1.V22.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ChurinDV.Sprint4.Task1.V22.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ba700f7c-d00f-4ddb-8e10-053047c8a0c2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
